97207b9b12e40300200805bf3dc276a84413fa6a
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-06-07 18:14:32 +0300

    user.js + app.js done

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//1.
 const http = require("http");
 http.createServer(function(reuqest, response)
 {
@@ -10,12 +9,20 @@ http.createServer(function(reuqest, response)
 //D:\Progs\NodeJS\node.exe .\app.js
 //Server started in port 3000.
 
-//2.
 const os = require("os");
 const greeting = require("./greeting");
 let userName = os.userInfo().username;
 console.log(`Data Request: ${greeting.date}`);
 console.log(greeting.getMessage(userName));
+const User = require("./user.js");
 //D:\Progs\NodeJS\node.exe .\app.js
 //Data Request: Fri Jun 07 2024 18:06:07 GMT+0300 (Eastern European Summer Time)
 //Good Evening, SuperDuperPCsasa
+
+let user1 = new User("Svarog", 35);
+user1.sayHi();
+//D:\Progs\NodeJS\node.exe .\app.js
+//Data Request: Fri Jun 07 2024 18:13:33 GMT+0300 (Eastern European Summer Time)
+//Good Evening, SuperDuperPCsasa
+//Hail Wotan! My name is Svarog
+//Server started in port 3000.
