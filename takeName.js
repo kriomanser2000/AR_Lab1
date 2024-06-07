8aee2c8e837942ef011e2550fea803433cadab6c
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-06-07 17:57:14 +0300

    takeName.js done

---
+++ takeName.js
@@ -0,0 +1,6 @@
+const http = require("http");
+const os = require("os");
+let userName = os.userInfo().username;
+console.log(userName);
+//D:\Progs\NodeJS\node.exe .\takeName.js
+//SuperDuperPCsasa
